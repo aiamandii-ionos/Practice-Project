a7d18284f5201af0260f4a328c3665d6e2ad121b
aiamandii <ana-maria.iamandii@ionos.com>
2022-05-16 15:14:47 +0300

    added server dto with validation, server mapper, custom validation

---
+++ src/main/java/org/acme/dto/ServerDto.java
@@ -0,0 +1,24 @@
+package org.acme.dto;
+
+import org.acme.validator.RamConstraint;
+
+import java.util.UUID;
+
+import javax.validation.constraints.*;
+
+public record ServerDto (
+        UUID id,
+        @NotBlank
+        String name,
+
+        @NotNull @Min(value = 1, message = "number must be positive")
+        Integer cores,
+
+        @NotNull @RamConstraint
+        Integer ram,
+
+        @NotNull @Min(value = 1, message = "number must be positive")
+        Integer storage
+)
+{
+}
---
+++ src/main/java/org/acme/validator/RamConstraint.java
@@ -0,0 +1,19 @@
+package org.acme.validator;
+
+import java.lang.annotation.*;
+
+import javax.validation.*;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+@Target({ FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = RamValidator.class)
+@Documented
+public @interface RamConstraint {
+    String message() default "number must be a multiple of 1024 MB";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
